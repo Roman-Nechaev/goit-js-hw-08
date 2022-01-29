5f6bb12481e85f388cc683e8cc87525648c25a85
Roman-Nechaev <nrchaevroman@gmail.com>
2022-01-29 16:38:18 +0200

    task-1-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Change code below this line
+
+const galleryListRef = document.querySelector('.gallery');
+galleryListRef.addEventListener('click', onOpenModal);
+
+const galleryItem = creatGalleryMark(galleryItems);
+galleryListRef.insertAdjacentHTML('beforeend', galleryItem);
+
+function creatGalleryMark(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+            <a  class="gallery__item" 
+            href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}" />
+          </a>
+  `;
+    })
+    .join('');
+}
+
+function onOpenModal(event) {
+  event.preventDefault();
+}
+
+var onGallerySlider = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: `250`,
+});
